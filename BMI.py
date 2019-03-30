2517bad3e90ddf851b0affbb2fefcfdcd2a14abb
Panfil <PANFIPR1@novartis.net>
2019-03-31 00:14:11 +0100

    Initial commit

---
+++ BMI.py
@@ -0,0 +1,70 @@
+print("Kalkulator BMI")
+w=int()
+h=int()
+
+
+# def input_weight():
+#     w=int(input("wprowadź swoją wagę w[kg]"+"  "))
+#     try:
+#         w=float(w)
+#         w=int(w)
+#
+#         # if w<10:
+#         #     print("Chyba się pomyliłeś. To niemożliwe, że tyle ważysz!\nChcesz poprawić wprowadzoną wartość? ")
+#         #     d=input("T/N"+ " ")
+#         #     if d=="t":
+#         #         input_weight()
+#         #     elif d=="n":
+#         #         print("as u wish")
+#         #         input_height(h)
+#         # elif w>175:
+#         #     print("To nie możliwe, żeś taki olbrzym")
+#         # else:
+#         #     print("Twoja waga to", w)
+#
+#     except:
+#         print("wprowadzona wartość jest nieprawidłowa")
+#         input_weight()
+#
+# def input_height():
+#     h=input("wprowadź swój wzrost w [cm]"+"  ")
+#     try:
+#         h=float(h)
+#         h=int(h)
+#         if h<120:
+#             print("Chyba się pomyliłeś!\nChcesz poprawić wprowadzoną wartość? ")
+#             d=input("T/N"+ " ")
+#             if d=="t":
+#                 input_height()
+#             elif d=="n":
+#                 print("as u wish")
+#                 count(r)
+#         elif h>215:
+#             print("To nie możliwe, żeś taki olbrzym")
+#         else:
+#             print("Twój wzrost to", h)
+#     except:
+#         print("wprowadzona wartość jest nieprawidłowa")
+#         input_height()
+#
+#
+#
+#
+# # def count(r):
+# #     r=(w/((h/100)**2))
+# # count(r)
+#
+# input_weight()
+# input_height()
+# print(w)
+# print(h)
+# # if r<18.50:
+# #     print("masz niedowagę")
+# # elif r>=24.99:
+# #     print("masz poważną nadwagę")
+# # else:
+# #     print("dobrze się prowadzisz")
+
+
+
+
